575a3d9dc1487bf5e519d36a0f815ea9ba5cc4bd
rautrot <tentacle0511@gmail.com>
2015-04-07 01:57:27 +0900

    test

--- tjslist.js
+++ tjslist.js
@@ -146,11 +146,11 @@ function serch(list, files){
           }
         }
       }
-      console.log(arr);
       //sumationを計算
       for(var i = 1;i < arr.length;i++){
         if(arr[i] == "" || arr[i-1] == "")continue;
         for(var j = 2;j < arr[i].length;j++){
+          console.log(arr[i][j]);
           arr[i][1] += arr[i][j];
         }
       }
