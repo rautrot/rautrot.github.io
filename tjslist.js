08f93aabfdde9b329ca5ff35820fe0251d06a443
rautrot <tentacle0511@gmail.com>
2015-04-06 18:04:04 +0900

    てｓｔ

--- tjslist.js
+++ tjslist.js
@@ -88,8 +88,8 @@ function serch(list, files){
   }
     //ファイル名の追加
 
-    console.log(typeof(files[i].name));
-    arr[0].push(files[i].name.toString());
+    var fileName = files[i].name;
+    arr[0].push(fileName);
 
     //ファイルの中身を引き出し，chrに格納
     reader.readAsText(files[i],'UTF-8');
