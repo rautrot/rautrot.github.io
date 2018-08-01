1737c3959fc317279b957a91fa1b5acb3aad541a
rautrot <tentacle0511@gmail.com>
2018-08-01 15:55:24 +0900

    gray-scott用完成
    整数値全てに対応(Lから-Lまで)

--- display_gray-scott/main.js
+++ display_gray-scott/main.js
@@ -4,10 +4,11 @@
 // ファイル入力処理
 //    先頭においちゃう？
 
-document.forms.id_form1.id_textBox1.value = "1,0,0,1,0,1\n0,1,1,0,1,0\n1,0,1,0,1,0";
+document.forms.id_form1.id_textBox1.value = "1,0,0,2,0,1\n0,1,-2,0,1,0\n1,0,1,0,2,0";
 
 var WIDTH = 1000; // キャンバスの横幅
 var HEIGHT = 1000; // キャンバスの高さ
+var L = 0; //数値の最大値を格納
 
 // ファイル入力フォーム
 var form = document.forms.f_input;
@@ -15,9 +16,24 @@ var form = document.forms.f_input;
 // Webページ読み込み時
 onload = function() {
 
-      var ctx = document.getElementById('canvassample').getContext('2d');
-      ctx.fillStyle='rgb(100,100,100)';
-      ctx.fillRect(0,0,WIDTH,HEIGHT);
+  // カラーサンプル
+  var ctx1 = document.getElementById('colorsample').getContext('2d');
+  //黒から緑まで
+  var i;
+  for(i=0;i<400;i++){
+    ctx1.fillStyle='rgb(' + Math.ceil(i/4) + ',' + Math.ceil(2*i/4) + ',' + Math.ceil(i/4) + ')';
+    ctx1.fillRect(i+25,0,4,30);
+  }
+  //文字の表示
+  ctx1.fillStyle=('rgb(0,0,0)');
+  ctx1.font = "20px 'ＭＳ　ゴシック'";
+  ctx1.fillText('-L',0,20,50);
+  ctx1.fillText('L',435,20,50);
+
+  // キャンバスの初期化
+  var ctx = document.getElementById('canvassample').getContext('2d');
+  ctx.fillStyle='rgb(100,100,100)';
+  ctx.fillRect(0,0,WIDTH,HEIGHT);
 
 
 };
@@ -52,13 +68,14 @@ form.input_file.addEventListener('change', function(e){
 
     document.forms.id_form1.id_textBox1.value = reader.result;
 
-    // バイナリデータの配列へ入力
+    // バイナリデータの配列へ入力と最大値の取得
     var binary_data = new Array();
     for(i=0;i<Y_num;i++){
         binary_data[i] = new Array();
         tmp_data_array = data[i].split(",");
         for(j=0;j<X_num;j++){
             binary_data[i][j] = tmp_data_array[j];
+            if(L < Number(tmp_data_array[j]))L = Number(tmp_data_array[j]);
         }
     }
 
@@ -91,7 +108,8 @@ function onButtonClick(){
       binary_data[i] = new Array();
       tmp_data_array = data[i].split(",");
       for(j=0;j<X_num;j++){
-          binary_data[i][j] = tmp_data_array[j];
+          binary_data[i][j] = Number(tmp_data_array[j]);
+          if(L < Number(tmp_data_array[j]))L = Number(tmp_data_array[j]);
       }
   }
 
@@ -118,11 +136,10 @@ function draw(binary_data, X_num, Y_num) {
   ctx.fillRect(0,0,canvas.width,canvas.height);
   for(i=0;i<Y_num;i++){
       for(j=0;j<X_num;j++){
-          if(binary_data[i][j]==1){
-            /* 2Dコンテキスト */
-            ctx.fillStyle='rgb(100,200,100)';
-            ctx.fillRect(j*delta_x,i*delta_y,Math.ceil(delta_x),Math.ceil(delta_y));
-          }
+          var val = binary_data[i][j];
+          val = ((val + L) / (2*L)) * 122.5;
+          ctx.fillStyle='rgb(' + Math.ceil(val) + ',' + Math.ceil(2*val) + ',' + Math.ceil(val) + ')';
+          ctx.fillRect(j*delta_x,i*delta_y,Math.ceil(delta_x),Math.ceil(delta_y));
       }
   }
 };
